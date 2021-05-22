4028763453e51b18c8ae1ad8b14d7b3c87874d85
Javadi520 <Javadi5520@Gmail.com>
2021-05-22 23:31:58 +0430

    Show Comment in article details implemented

--- MB.Infrastructure.Query/ArticleQuery.cs
+++ MB.Infrastructure.Query/ArticleQuery.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using MB.Domain.CommentAgg;
 using MB.Infrastructure.EFCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,15 +19,20 @@ namespace MB.Infrastructure.Query
 
         public List<ArticleQueryView> GetArticles()
         {
-            return _context.Articles.Include(x => x.ArticleCategory).Select(x => new ArticleQueryView
-            {
-                Id = x.Id,
-                Title = x.Title,
-                ArticleCategory = x.ArticleCategory.Title,
-                CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
-                ShortDescription = x.ShortDescription,
-                Image = x.Image
-            }).ToList();
+            return _context.Articles
+                .Include(x => x.ArticleCategory)
+                .Include(x => x.Comments)
+                .Select(x =>
+                    new ArticleQueryView
+                    {
+                        Id = x.Id,
+                        Title = x.Title,
+                        ArticleCategory = x.ArticleCategory.Title,
+                        CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
+                        ShortDescription = x.ShortDescription,
+                        Image = x.Image,
+                        CommentCount = x.Comments.Count(x => x.Status == Statuses.Confirmed)
+                    }).ToList();
         }
 
         public ArticleQueryView GetArticle(long id)
@@ -38,8 +45,20 @@ namespace MB.Infrastructure.Query
                 CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
                 ShortDescription = x.ShortDescription,
                 Image = x.Image,
-                Content = x.Content
+                Content = x.Content,
+                CommentCount = x.Comments.Count(x => x.Status == Statuses.Confirmed),
+                Comments = MapComments(x.Comments.Where(z => z.Status == Statuses.Confirmed))
             }).FirstOrDefault(x => x.Id == id);
+        }
+
+        private static List<CommentQueryView> MapComments(IEnumerable<Comment> comments)
+        {
+            return comments.Select(comment => new CommentQueryView
+            {
+                Name = comment.Name,
+                CreationDate = comment.CreationDate.ToString(CultureInfo.InvariantCulture),
+                Message = comment.Message
+            }).ToList();
         }
     }
 }
